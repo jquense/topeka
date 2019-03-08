21562f6505068ffec674c07b0ca6f245103460b2
Jason Quense <monastic.panic@gmail.com>
2019-03-08 16:34:18 -0500

    feat: add hook
    BREAKING CHANGE: bump min react version for hooks

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import BindingContext from './BindingContext';
-import Binding from './Binding';
-import updateIn from './updateIn';
-
-export {
-  Binding,
-  BindingContext,
-  updateIn
-}
--- test/bindings.test.js
+++ test/bindings.test.js
@@ -1,8 +1,9 @@
-import React from 'react'
+/* eslint-disable react/prop-types */
+import React, { useEffect } from 'react'
 import sinon from 'sinon'
 import { mount } from 'enzyme'
 
-import { Binding, BindingContext } from '../src'
+import { Binding, BindingContext, useBinding } from '../src'
 
 describe('Bindings', () => {
   class StaticContainer extends React.Component {
@@ -14,12 +15,37 @@ describe('Bindings', () => {
     }
   }
 
-  it('should update the form value', function() {
+  const BoundInput = ({ name }) => {
+    const [value = '', handleChange] = useBinding(name)
+    return <input type="text" value={value} onChange={handleChange} />
+  }
+
+  it('should update the form value: hook', function() {
     let change = sinon.spy()
+
+    let inst = mount(
+      <BindingContext onChange={change}>
+        <BoundInput name="name" />
+      </BindingContext>
+    )
+
+    inst
+      .find('input')
+      .first()
+      .simulate('change', { target: { value: 'Jill', tagName: 'DIV' } })
+
+    change.should.have.been.calledOnce.and.calledWith({ name: 'Jill' })
+  })
+
+  it('should update the form value: Binding', function() {
+    let change = sinon.spy()
+
     let inst = mount(
       <BindingContext onChange={change}>
         <Binding bindTo="name">
-          {props => <input type="text" {...props} />}
+          {(value, onChange) => (
+            <input value={value || ''} onChange={onChange} {...this.props} />
+          )}
         </Binding>
       </BindingContext>
     )
@@ -32,39 +58,23 @@ describe('Bindings', () => {
     change.should.have.been.calledOnce.and.calledWith({ name: 'Jill' })
   })
 
-  it('should handle function children', function() {
-    let change = sinon.spy()
-
-    mount(
-      <BindingContext onChange={change}>
-        <Binding bindTo="name">
-          {props => <input type="text" {...props} />}
-        </Binding>
-      </BindingContext>
-    )
-      .find('input')
-      .first()
-      .simulate('change', { target: { value: 'Jill', tagName: 'DIV' } })
-
-    change.should.have.been.calledOnce.and.calledWith({ name: 'Jill' })
-  })
-
-  it.only('should always update if binding value changed', function() {
+  it('should always update if binding value changed', function() {
     let change = sinon.spy()
     let value = { name: 'sally', eyes: 'hazel' }
     let count = 0
 
-    class Input extends React.Component {
-      componentDidUpdate() {
+    const CountRenders = ({ name }) => {
+      const [value = '', handleChange] = useBinding(name)
+      useEffect(() => {
         count++
-      }
-      render = () => <input type="text" {...this.props} />
+      })
+      return <input type="text" value={value} onChange={handleChange} />
     }
 
     let wrapper = mount(
       <BindingContext onChange={change} value={value}>
         <StaticContainer shouldUpdate={false}>
-          <Binding bindTo="name">{props => <Input {...props} />}</Binding>
+          <CountRenders name="name" />
         </StaticContainer>
       </BindingContext>
     )
@@ -80,18 +90,17 @@ describe('Bindings', () => {
     count.should.equal(2)
   })
 
-  it.only('should update if props change', function() {
+  it('should update if props change', function() {
     let count = 0
-    class Input extends React.Component {
-      componentDidUpdate() {
+    const CountRenders = ({ name }) => {
+      const [value = '', handleChange] = useBinding(name)
+      useEffect(() => {
         count++
-      }
-      render = () => <input type="text" {...this.props} />
+      })
+      return <input type="text" value={value} onChange={handleChange} />
     }
 
-    let wrapper = mount(
-      <Binding bindTo="name">{props => <Input {...props} />}</Binding>
-    )
+    let wrapper = mount(<CountRenders bindTo="name" />)
 
     count.should.equal(0)
 
@@ -117,7 +126,9 @@ describe('Bindings', () => {
         return (
           <BindingContext onChange={change} value={value}>
             <Binding bindTo="name">
-              {props => <Input {...props} {...this.props} />}
+              {(value, onChange) => (
+                <Input value={value} onChange={onChange} {...this.props} />
+              )}
             </Binding>
           </BindingContext>
         )
